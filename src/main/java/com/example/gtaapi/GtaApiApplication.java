7a23527b02ada49d97f79f5e9e73b040a585baf6
Tyler Ross <Tyler_Ross3@Baylor.edu>
2022-08-13 12:16:28 -0500

    inital commit

---
+++ src/main/java/com/example/gtaapi/GtaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gtaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtaApiApplication.class, args);
+    }
+
+}
