0b60ae183794b03cb949375147d52fbeca488f28
Tyler Ross <Tyler_Ross3@Baylor.edu>
2022-08-14 21:47:36 -0500

    Annotations issue

--- src/main/java/com/gtaapi/config/AppConfig.java
+++ src/main/java/com/gtaapi/config/AppConfig.java
@@ -3,7 +3,6 @@ package com.gtaapi.config;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -15,6 +14,5 @@ import org.springframework.stereotype.Component;
 @Getter @Setter
 public class AppConfig {
 
-    @NotNull
     public String url;
 }
