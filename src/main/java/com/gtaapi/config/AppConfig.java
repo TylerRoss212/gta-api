9e7496a0a1140bf601a5022e991f70bd86e2ee46
Tyler Ross <Tyler_Ross3@Baylor.edu>
2022-08-15 18:22:16 -0500

    Database update

--- src/main/java/com/gtaapi/config/AppConfig.java
+++ src/main/java/com/gtaapi/config/AppConfig.java
@@ -1,9 +1,7 @@
 package com.gtaapi.config;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
